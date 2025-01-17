2d4054c40ce756209622c0e96654c289694f8c73
Megadino <megacom.net@gmail.com>
2025-01-17 21:09:39 +0100

    deleted unknown acc

--- Solnet.Raydium/Consts.cs
+++ Solnet.Raydium/Consts.cs
@@ -40,9 +40,7 @@ namespace Solnet.Raydium
 
         public const string PumpFun = "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P";
 
-        public const string MetaplexTokenMetadata = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s";
-
-        public const string EventAuthority = "Ce6TQqeHC9p8KetsN6JsjHK7UTZk7nasjjnr7XxXp9F1";
+        public const string MetaplexTokenMetadata = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s";        
 
         public static readonly PublicKey SYSVAR_CLOCK_PUBKEY = new("SysvarC1ock11111111111111111111111111111111");
 
